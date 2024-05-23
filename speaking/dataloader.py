20bf900b93026338e3f37a079c391c009c7df89e
Gmaan2 <miguel.a.gerena@gmail.com>
2024-05-23 19:10:54 -0400

    speak multiple epochs run to get perf trend

--- speaking/dataloader.py
+++ speaking/dataloader.py
@@ -5,37 +5,23 @@ from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import os
 from torchvision import transforms as t
-
-video_path = "path_to_a_test_video"
-reader = torchvision.io.VideoReader(video_path, "video")
-reader.seek(2.0)
-frame = next(reader)
-
-
-reader.seek(2)
-for frame in reader:
-    frames.append(frame['data'])
-# additionally, `seek` implements a fluent API, so we can do
-for frame in reader.seek(2):
-    frames.append(frame['data'])
-
-
-# and similarly, reading 10 frames after the 2s timestamp can be achieved as follows:
-    for frame in itertools.islice(reader.seek(2), 10):
-    frames.append(frame['data'])
+import numpy as np
+import math
+import torch
 
 class LipReadingDataset(Dataset):
-    def __init__(self, directory, transform=None, resolution=0.5):
+    def __init__(self, directory, transform=None, resolution=0.5, length_video=200):
         """
         Args:
             directory (string): Directory with all the video folders.
             transform (callable, optional): Optional transform to be applied on a sample.
         """
         self.directory = directory
-        transform = [t.ToTensor(), t.Resize((int(180*resolution), int(180*resolution))), ]  # 90x90
+        transform = [t.ToPILImage(), t.ToTensor()]  # 90x90
         self.transform  = t.Compose(transform)
         self.samples = self._load_samples()
         self.resolution = resolution
+        self.length_video = length_video
 
     def _load_samples(self):
         samples = []
@@ -47,20 +33,62 @@ class LipReadingDataset(Dataset):
 
             for sub_folder in sub_folders:
                 sub_folder_path = os.path.join(video_path, sub_folder)
+
                 video_dir = os.path.join(sub_folder_path, sub_folder + '.mp4')
                 label_file = os.path.join(sub_folder_path, sub_folder + '.txt')
-                 
-                # print("Checking:", sub_folder_path)  
 
-                if os.path.exists(label_file) and os.path.isdir(video_dir):
-                    label = self.read_phrase_from_file(label_file)
-                    frames = [os.path.join(video_dir, f) for f in sorted(os.listdir(video_dir)) if f.endswith('.jpg')]
-                    samples.append((frames, label))
-                # else:
-                #     print("Skipped:", sub_folder_path)  
-
-            # if os.getlogin() == "darke" and len(samples) > 1000:
-            # if len(samples) > 100:
-            #     break
-
+                if os.path.exists(label_file) and os.path.exists(video_dir):
+                    samples.append((video_dir, label_file))
         return samples
+    
+    
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        video_path, label = self.samples[idx]
+        reader = torchvision.io.read_video(video_path, pts_unit = 'sec', output_format='TCHW')
+        labels_np = torch.zeros((self.length_video), dtype=torch.int)
+        labels_np = self.get_labels(label, labels_np,  reader[-1]["video_fps"])
+        c,h,w = reader[0].shape[1:]
+        frames = torch.zeros((self.length_video, c, h, w), dtype=torch.float32)
+        # frames_path, label = self.samples[0]
+        for i, frame in enumerate(reader[0]):
+            frames[i] = self.transform(frame)
+            if i == self.length_video -1:
+                break
+        frames = frames.permute(1,0,2,3)
+        return frames, labels_np.type(torch.LongTensor), video_path
+    
+    def get_labels(self, label_file, labels_np, frame_rate):
+        with open(label_file, "r") as F:
+            lines = F.read()
+            lines = lines.splitlines()
+            is_start_line = False
+            for line in lines:
+                if not is_start_line:
+                    if line == "WORD START END ASDSCORE":
+                        is_start_line = True
+                    continue
+                _, curr_start, curr_end, _ = line.split(" ")
+
+                curr_start = math.ceil(float(curr_start) * frame_rate)
+                curr_end = math.floor(float(curr_end) * frame_rate)
+                labels_np[curr_start: curr_end + 1] = 1
+                if curr_end == self.length_video:
+                    break
+        return labels_np
+
+if __name__ == "__main__":
+    train_dataset = LipReadingDataset(directory='./LRS2/data_splits/train' if os.getlogin() != "darke" else "D:/classes/project/LRS2/data_splits/train", transform=None)
+    data_loader = DataLoader(
+                train_dataset,
+                batch_size=1,
+                shuffle=True,
+                # collate_fn=collate_fn,
+                num_workers=1,
+                pin_memory=False,
+                )
+    for batch_idx, (frames, targets) in enumerate(data_loader):
+        print("")
+        
--- speaking/main.py
+++ speaking/main.py
@@ -1,56 +1,124 @@
-from torchvision.datasets import VisionDataset
-
 import torch
+from torch.utils.data import DataLoader
+from dataloader import LipReadingDataset
+from speak import Speaking_conv3d_layers
+import torch
+import os
+from tqdm import tqdm
 import argparse
-import tensorboard
-import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+import datetime
+from torch import nn
+from transformers import get_constant_schedule_with_warmup, get_cosine_schedule_with_warmup
+import torchvision
 
-def collate_fn(batch):
-    sequences, labels = zip(*batch)
-    encoded_labels = tokenizer(labels, add_special_tokens=True, max_length=100, padding="longest",  return_tensors='pt')
-    
-    return torch.unsqueeze(torch.stack(sequences[0]), axis=0), encoded_labels
+torch.manual_seed(42)
+
+def validate(loader, model, criterion, writer=None, total_len=0, idx=0, isWrite=False):
+    progress_bar = tqdm(loader, total=len(loader))
+    total_correct = total_words = 0
+    zeros = ones = 0
+    loss = 0
+    poor = []
+    if isWrite:
+        file = open("./speaking/analyze_data/val_perf_lower_than50.txt", "w")
+    for frames, label, video_path in progress_bar:
+        frames, label, video_path = frames.to(device), label.to(device), video_path[0]
+        with torch.no_grad():
+            output = model(frames)
+            correct = torch.sum(torch.argmax(output, axis=1) == label).detach().item()
+            total =  label.shape[1]
+            total_correct += correct
+            total_words += total# this only works if its 1 batch size since other wise they will be padded
+
+            if isWrite and correct/total <= 0.5:
+                # file.write(f"{video_path} : {correct/total}\n")
+                reader = torchvision.io.read_video(video_path, pts_unit = 'sec', output_format='TCHW')
+                poor.append((correct/total,  reader[0].shape[0]/25, video_path))
+            loss += criterion(output, label).detach().item() 
+            ones += torch.sum(label==1)
+            zeros += torch.sum(label==0)
+    total = ones + zeros
+    if isWrite:
+        file.writelines(str(sorted(poor)))
+        file.close()
+
+    print(f"ones: {ones}, zeros: {zeros}, total: {total}, one_frac: {ones/total } zeors_frac: {zeros/total}")
+    print(f"total_correct: {total_correct} total_words: {total_words}")
+    print(f"accuracy: {total_correct/total_words}")
+    print(f"loss: {loss}")
+
+    if writer:
+        writer.add_scalar('Val Accuracy', total_correct/total_words, total_len + idx)
+        writer.add_scalar('loss Accuracy', loss, total_len + idx)
 
 
 
 def main(args):
+    length_video = 125 
     now = datetime.datetime.now()
-    model = LipReadingModel()
+    model = Speaking_conv3d_layers(512, length_video)
     model.to(device)
+    weight = torch.tensor([0.77, 0.23]).to(device)
+    criterion = nn.CrossEntropyLoss(weight=weight)
+    train_dataset = LipReadingDataset(directory='./LRS2/data_splits/train' if os.getlogin() != "darke" else "D:/classes/project/LRS2/data_splits/train",
+                                transform=None,
+                                length_video=length_video)
+    print("Total samples loaded:", len(train_dataset))  
+    train_set, val_set = torch.utils.data.random_split(train_dataset, [int(len(train_dataset)*0.9), len(train_dataset) - int(len(train_dataset)*0.9)])
+
+    train_data_loader = DataLoader(
+        train_set,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.num_workers,
+        pin_memory=False,
+        )
+
+    val_data_loader = DataLoader(
+        val_set,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.num_workers,
+        pin_memory=False,
+        )
+    print("Total samples train:", len(train_data_loader))  
+    print("Total samples val:", len(val_data_loader))  
+
 
     if args.train:
         save_path = f'{args.data_type}/Batch_size_{args.batch_size}/LR_{args.learning_rate}/Date_{now.month}_{now.day}_hr_{now.hour}'
         os.makedirs(save_path, exist_ok=True)
-        writer = SummaryWriter(f'runs/{save_path}')
+        writer = SummaryWriter(f'runs_speak/{save_path}')
 
-        train_dataset = LipReadingDataset(directory='./LRS2/data_splits/train' if os.getlogin() != "darke" else "D:/classes/cs231n/project/LRS2/data_splits/train", transform=None)
-        print("Total samples loaded:", len(train_dataset))  
-        data_loader = DataLoader(
-            train_dataset,
-            batch_size=args.batch_size,
-            shuffle=True,
-            collate_fn=collate_fn,
-            num_workers=args.num_workers,
-            pin_memory=False,
-            )
+
+        
         
         optimizer = torch.optim.AdamW(model.parameters(), betas=(0.99, 0.95), lr=args.learning_rate)
         # scheduler = get_constant_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=100)
-        scheduler = get_cosine_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=100, num_training_steps=int(len(train_dataset)/args.grad_accum_steps))
-        criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)  # Outputs: [batch_size, num_classes, sequence_length] Targets: [batch_size, sequence_length]
+        scheduler = get_cosine_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=100, num_training_steps=int(len(train_data_loader)/args.grad_accum_steps))
+  # Outputs: [batch_size, num_classes, sequence_length] Targets: [batch_size, sequence_length]
+
+        if args.resume:
+            print("Resuming training...")
+            checkpoint = torch.load("0.state")
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            scheduler = get_cosine_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=100, num_training_steps=int(len(train_data_loader)/args.grad_accum_steps), last_epoch=1)
+
 
         for epoch in range(args.epochs):
             model.train() 
             total_loss = 0
             avg_loss = loss_accum = 0
             
-            progress_bar = tqdm(enumerate(data_loader), total=len(data_loader), desc=f'Epoch {epoch+1}/{args.epochs}')
-            for batch_idx, (frames, targets) in progress_bar:
-                frames, input_id, mask = frames.to(device, non_blocking=True), targets['input_ids'].to(device, non_blocking=True), targets['attention_mask'].to(device, non_blocking=True)
+            progress_bar = tqdm(enumerate(train_data_loader), total=len(train_data_loader), desc=f'Epoch {epoch+1}/{args.epochs}')
+            for batch_idx, (frames, label, _) in progress_bar:
+                frames, label = frames.to(device, non_blocking=True), label.to(device, non_blocking=True)
             
-                output = model(frames, input_id, mask, args.train)
+                output = model(frames)
 
-                loss = criterion(output, input_id) 
+                loss = criterion(output, label) 
                 loss_accum += loss
                 loss = loss.detach()
                 total_loss += loss
@@ -64,15 +132,18 @@ def main(args):
                     loss_accum = 0
                     optimizer.step()
                     scheduler.step()
+                    # if batch_idx % int(len(train_data_loader) * 0.2) == 0:
+                    #     model.eval()
+                    #     validate(val_data_loader, model, writer, epoch * len(train_data_loader), batch_idx)
+                    #     model.train()
 
-                writer.add_scalar('Training Loss', loss, epoch * len(data_loader) + batch_idx)
-                writer.add_scalar('Average Batch Loss', avg_loss/(batch_idx+1), epoch * len(data_loader) + batch_idx)
-                del loss, frames, mask, targets
+                writer.add_scalar('Learning rate ', torch.tensor(scheduler.get_last_lr()), epoch * len(train_data_loader) + batch_idx)
+                writer.add_scalar('Training Loss', loss, epoch * len(train_data_loader) + batch_idx)
 
+                writer.add_scalar('Average Batch Loss', avg_loss/(batch_idx+1), epoch * len(train_data_loader) + batch_idx)
+                del loss, frames
                 
-                print("target \n",tokenizer.batch_decode(input_id))
-                print("Guess \n",tokenizer.batch_decode(torch.argmax(output, dim=1)))
-            average_loss = total_loss / len(data_loader)
+            average_loss = total_loss / len(train_data_loader)
             writer.add_scalar('Average Training Loss', average_loss, epoch)
             print(f"Average Loss for Epoch {epoch}: {average_loss}")
             state = {
@@ -81,6 +152,15 @@ def main(args):
                 'optimizer': optimizer.state_dict(),
                 }
             torch.save(state, f"{epoch}.state")
+            model.eval()
+            validate(val_data_loader, model, criterion, writer, epoch * len(train_data_loader), batch_idx)
+
+    else:
+        model.load_state_dict(torch.load("0.state")['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        model.eval()
+        validate(val_data_loader, model, criterion, epoch * len(train_data_loader), batch_idx)
+
 
 
 
@@ -99,8 +179,11 @@ if __name__ == "__main__":
 
     parser.add_argument('--num_workers', default=4, type=int, help='num of workes for the dataloader')
 
-    parser.add_argument('--learning_rate', default=0.001, type=int, help='learning rate for optimizer')
+    parser.add_argument('--learning_rate', default=6e-4, type=int, help='learning rate for optimizer')
     # 3e-4 
-    parser.add_argument('--epochs', default=2, type=int, help='num epoch to train for')
+    parser.add_argument('--epochs', default=12, type=int, help='num epoch to train for')
+    parser.add_argument('--resume', default=True, type=bool, help='resume training')
+
+    
     args = parser.parse_args()
     main(args)
